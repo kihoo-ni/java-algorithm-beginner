45640c9ba9cbfa88a02a632c507f98cae0d68da7
kihoo-ni <gudcjf532@gmail.com>
2024-04-13 20:09:27 +0900

    4.13 [큐 덱(양방향대기열) 연습문제 구현]

--- practice04/src/main/java/com/example/IntDeque.java
+++ practice04/src/main/java/com/example/IntDeque.java
@@ -22,76 +22,135 @@ public class IntDeque {
 
     // --- 생성자 ---//
     public IntDeque(int maxlen) {
-
+        capacity = maxlen;
+        num = front = rear = 0;
+        que = new int[capacity];
     }
 
     // --- 덱의 맨앞에 데이터를 인큐 ---//
     public int enqueFront(int x) throws OverflowIntDequeException {
+        if (num >= capacity) {
+            throw new OverflowIntDequeException();
+        }
 
+        num++;
+        if (--front < 0) {
+            front = capacity - 1;
+        }
+        que[front] = x;
+        return x;
     }
 
     // --- 덱의 맨끝에 데이터를 인큐 ---//
     public int enqueRear(int x) throws OverflowIntDequeException {
-
+        if (num >= capacity) {
+            throw new OverflowIntDequeException();
+        }
+        que[rear++] = x;
+        num++;
+        if (rear == capacity) {
+            rear = 0;
+        }
+        return x;
     }
 
     // --- 덱의 맨앞 데이터를 디큐 ---//
     public int dequeFront() throws EmptyIntDequeException {
+        if (num <= 0) {
+            throw new EmptyIntDequeException();
+        }
+        int x = que[front++];
+        num--;
+        if (front == capacity) {
+            front = 0;
+        }
 
+        return x;
     }
 
     // --- 덱의 맨끝 데이터를 디큐 ---//
     public int dequeRear() throws EmptyIntDequeException {
+        if (num <= 0) {
+            throw new EmptyIntDequeException();
+        }
+        num--;
+        if (--rear < 0) {
+            rear = capacity - 1;
+        }
+        return que[rear];
 
     }
 
     // --- 덱의 맨앞 데이터를 피크(맨앞 데이터를 들여다봄 ) ---*/
     public int peekFront() throws EmptyIntDequeException {
+        if (num <= 0) {
+            throw new EmptyIntDequeException();
+        }
 
+        return que[front];
     }
 
     // --- 덱의 맨끝에서 데이터를 피크(맨끝 데이터를 들여다봄 ) ---*/
     public int peekRear() throws EmptyIntDequeException {
-
+        if (num <= 0) {
+            throw new EmptyIntDequeException();
+        }
+        return que[rear == 0 ? capacity - 1 : rear - 1];
     }
 
     // --- 덱에서 x를 검색하여 인덱스(발견하지 못하면 -1)를 반환합니다 ---//
     public int indexOf(int x) {
-
+        for (int i = 0; i < num; i++) {
+            int idx = (front + i) % capacity;
+            if (que[idx] == x) {
+                return idx;
+            }
+        }
+        return -1;
     }
 
     // --- 덱에서 x를 검색하여 맨앞에서 몇 번째인가(발견하지 못하면 0)를 반환합니다 ---//
     public int search(int x) {
+        for (int i = 0; i < num; i++) {
+            int idx = (front + i) % capacity;
+            if (que[idx] == x) {
+                return i + 1;
+            }
+        }
+        return 0;
 
     }
 
     // --- 덱을 비웁니다 ---//
     public void clear() {
-
+        num = rear = front = 0;
     }
 
     // --- 덱의 용량을 반환합니다 ---//
     public int getCapacity() {
-
+        return capacity;
     }
 
     // --- 덱에 쌓여있는 데이터수를 반환합니다 ---//
     public int size() {
-
+        return num;
     }
 
     // --- 덱이 비어 있는가? ---//
     public boolean isEmpty() {
-
+        return num <= 0;
     }
 
     // --- 덱이 가득 찼는가? ---//
     public boolean isFull() {
-
+        return num >= capacity;
     }
 
     // --- 덱 안의 모든 데이터를 맨앞 → 맨끝의 순서로 출력 ---//
     public void dump() {
-
+        for (int i = 0; i < num; i++) {
+            System.out.print(que[(i + front) % capacity] + " ");
+        }
+        System.out.println();
     }
 }
